74ffbae3f38a81fd86f2041d8cc110f0ea5a20f8
Flowing <bitoreieru@gmail.com>
2020-01-29 18:29:24 +0900

    added json unmarshaling

--- gslt.v
+++ gslt.v
@@ -1,8 +1,9 @@
 import net.http 
+import json
 
 fn main(){
-	steam_web_api_key := "YOUR_STEAM_WEB_API_KEY"
-	get_list(steam_web_api_key)
+	steam_web_api_key := ""
+	// get_list(steam_web_api_key)
 	generate_gslt(steam_web_api_key, 730, "V") // 730=CSGO
 }
 
@@ -12,16 +13,30 @@ fn get_list(key string){
 		printf("ERR : %v\n",err)
 		panic(err)
 	}
-	printf("RESPONSE : %s\n",res.text)
+	printf("LIST : %s\n",res.text)
+}
+
+struct GenerateGsltResponse { // {"response":{"steamid":"xxx","login_token":"xxx"}}
+	response GsltStruct [json:response]
+} 
+
+struct GsltStruct {
+	steamid string [json:steamid]
+	login_token string [json:login_token]
 }
 
 fn generate_gslt(key string, appid int, memo string){
 	api_url := "https://api.steampowered.com/IGameServersService/CreateAccount/v1/?key="+key+"&appid="+appid.str()+"&memo="+memo
 	res := http.post(api_url,"json") or {
 		printf("ERR : %v\n",err)
-		panic(err)
+		return
 	}
-	printf("RESPONSE : %s\n",res.text)
+	// println(res.text)
+	gslt := json.decode(GenerateGsltResponse, res.text) or {
+    	eprintln('Failed to recode JSON...')
+    	return 
+	} 
+	println(gslt.response)
 }
 
 // TODO : Delete GSLT...
